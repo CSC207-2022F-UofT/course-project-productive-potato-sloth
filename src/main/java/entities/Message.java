0ceac3ad61bf8c6e4eca37296dd13a448be83f66
Dawei-He2002 <dawei.he@mail.utoronto.ca>
2022-11-09 21:47:34 -0500

    Updated the ChatRoomInteractor class to not have extraneous initialization method (i.e. removed initializer, class now uses default object initializer)
    Added setter and getter methods to Message
    
    copied two UI classes from the password manager project to the UI package. These are classes I'm sure will be necessary in implementation.

--- src/main/java/entities/Message.java
+++ src/main/java/entities/Message.java
@@ -11,4 +11,27 @@ public class Message {
         this.timestamp = LocalDateTime.now();
     }
 
+    /**Below are the setter and getter methods.
+     *
+     * Overriding the toString method may be the most convenient way of getting the message text.*/
+    @Override
+    public String toString(){
+        return this.content;
+    }
+
+    /**This method sets the content string to a new string. This may be necessary for editing messages.
+     *
+     * @param content: the String representing the new contents of the message.*/
+    public void setContent(String content){
+        this.content = content;
+    }
+
+    public String getAuthor(){
+        return this.author;
+    }
+
+    public LocalDateTime getDateTime(){
+        return this.timestamp;
+    }
+
 }
--- src/main/java/usecases/ChatRoomInteractor.java
+++ src/main/java/usecases/ChatRoomInteractor.java
@@ -4,17 +4,12 @@ import entities.*;
 
 public class ChatRoomInteractor {
     ChatRoom room;
-    public ChatRoomInteractor(ChatRoom room) {
-        this.room = room;
-        //Is this right? Please check. Because we have to figure out if the program is initialized from
-        //the "bottom" layers (i.e. lower layers get called first) or "top" layers, and in the latter case
-        //this approach won't be correct.
-    }
+
     public void sendMessage(String message/*, User user*/){
         Message temp_message = new Message(message/*, user*/);
         this.room.AddMessage(temp_message);
         /*ChatRoomPresenterInterface.updateView();
-        * --this line both saves to a .txt file and updates the view according to the file. Itmay be split into 2
+        * --this line both saves to a .txt file and updates the view according to the file. It may be split into 2
         * separate methods later.*/
     }
 }
