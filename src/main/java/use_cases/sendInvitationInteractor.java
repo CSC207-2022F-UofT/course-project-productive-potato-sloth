2b5a7de763f5660a3e905a2276ce22c49e6ab750
Khai Lam Nguyen <kldtntg@gmail.com>
2022-11-11 17:32:42 -0500

    Second - Entities, Use cases and Controllers

--- src/main/java/use_cases/sendInvitationInteractor.java
+++ src/main/java/use_cases/sendInvitationInteractor.java
@@ -1,17 +1,32 @@
 
 package use_cases;
 
+import entities.InvitationFactory;
 import entities.User;
 import entities.Invitation;
 
-public class sendInvitationInteractor {
+public class sendInvitationInteractor implements InvitationInputBoundary {
 
-    void sendInvitation(User sendingUser, User receivingUser){
+    final InvitationOutputBoundary presenter;
 
-        Invitation invitation = new Invitation(sendingUser, receivingUser);
+    final InvitationFactory invitationFactory; //maybe?
 
-        sendingUser.changeOutgoingInvitations(invitation); // add invitation
-        receivingUser.changeIncomingInvitations(invitation); // add invitation
+    public sendInvitationInteractor(InvitationOutputBoundary presenter, InvitationFactory invitationFactory) {
+        this.presenter = presenter;
+        this.invitationFactory = invitationFactory;}
+
+    @Override
+    public InvitationOutputModel sendInvitation(InvitationInputModel inputModel){
+
+
+        Invitation invitation = invitationFactory.create(inputModel.sender, inputModel.receiver, inputModel.task);
+        // depends on how
+        // changeOutgoingInvitations is implemented <=> may or may not use InvitationFactory here
+        inputModel.sender.changeOutgoingInvitations(invitation); // add invitation
+        inputModel.receiver.changeIncomingInvitations(invitation); // add invitation
+
+        InvitationOutputModel outputModel = new InvitationOutputModel(invitation.getSender(), invitation.getReceiver(), invitation.getTask());
+        return presenter.prepareSentView(outputModel);
 
     }
 }
