c1e20b3fac8f6e295f9d6c5ad26d75d070671535
Vishnu Nittoor <nitvishn@gmail.com>
2022-11-14 17:24:57 -0500

    Created Event, ScheduleEventInputData, and ScheduleEventInteractor. None of them do anything yet. Created an empty Tag object for now before I get Leon's.

--- src/main/java/entities/Event.java
+++ src/main/java/entities/Event.java
@@ -1,10 +1,39 @@
 package entities;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
 
 /*
 An entity class representing an Event
+TODO: document this class
  */
 public class Event {
     private LocalDateTime start_time;
+    private LocalDateTime end_time;
+    private List<User> collaborators;
+    private Task task;
+    private String name;
+    private boolean repeats;
+    private LocalTime frequency;
+    private List<Tag> tags;
+
+    public Event(LocalDateTime start_time,
+                 LocalDateTime end_time,
+                 List<User> collaborators,
+                 Task task,
+                 String name,
+                 boolean repeats,
+                 LocalTime frequency,
+                 List<Tag> tags
+    ) {
+        this.start_time = start_time;
+        this.end_time = end_time;
+        this.collaborators = collaborators;
+        this.task = task;
+        this.name = name;
+        this.repeats = repeats;
+        this.frequency = frequency;
+        this.tags = tags;
+    }
 }
