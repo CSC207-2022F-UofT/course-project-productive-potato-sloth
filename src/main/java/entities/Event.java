bd15f8427ec3df46683d8ede5b87f2aeac66ad14
Vishnu Nittoor <nitvishn@gmail.com>
2022-11-14 17:54:40 -0500

    Added setters and getters. Changed the Event class to store only usernames of collaborators to decrease coupling.

--- src/main/java/entities/Event.java
+++ src/main/java/entities/Event.java
@@ -11,16 +11,66 @@ TODO: document this class
 public class Event {
     private LocalDateTime start_time;
     private LocalDateTime end_time;
-    private List<User> collaborators;
-    private Task task;
+    private List<String> collaborator_usernames;
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+
+    private Task task; // optional, will be null if no task
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     private String name;
+
+    public boolean getRepeats() {
+        return repeats;
+    }
+
+    public void setRepeatFrequency(LocalTime repeatFrequency) {
+        this.repeats = true;
+        this.repeatFrequency = repeatFrequency;
+    }
+
+    public void clearRepeatFrequency(){
+        this.repeats = false;
+        this.repeatFrequency = null;
+    }
+
     private boolean repeats;
-    private LocalTime frequency;
+
+    public LocalDateTime getEndTime() {
+        return end_time;
+    }
+
+    public void setEndTime(LocalDateTime end_time) {
+        this.end_time = end_time;
+    }
+
+    public LocalDateTime getStartTime() {
+        return start_time;
+    }
+
+    public void setStartTime(LocalDateTime start_time) {
+        this.start_time = start_time;
+    }
+
+    private LocalTime repeatFrequency;
     private List<Tag> tags;
 
     public Event(LocalDateTime start_time,
                  LocalDateTime end_time,
-                 List<User> collaborators,
+                 List<String> collaborator_usernames,
                  Task task,
                  String name,
                  boolean repeats,
@@ -29,11 +79,11 @@ public class Event {
     ) {
         this.start_time = start_time;
         this.end_time = end_time;
-        this.collaborators = collaborators;
+        this.collaborator_usernames = collaborator_usernames;
         this.task = task;
         this.name = name;
         this.repeats = repeats;
-        this.frequency = frequency;
+        this.repeatFrequency = frequency;
         this.tags = tags;
     }
 }
