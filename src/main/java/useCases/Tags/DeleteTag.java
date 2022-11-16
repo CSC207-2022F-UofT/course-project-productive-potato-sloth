3b8e570757a074c1fed62c9055360e1c90a5081c
Dawei-He2002 <dawei.he@mail.utoronto.ca>
2022-11-16 14:01:48 -0500

    Fixed merge conflicts in Tag.java by git rm command.

---
+++ src/main/java/useCases/Tags/DeleteTag.java
@@ -0,0 +1,14 @@
+package useCases.Tags;
+
+/***
+ * A use case which deletes a tag from a user's tags
+ */
+public class DeleteTag {
+
+    public DeleteTag() {
+
+        // Requires data access object to be inplemented
+
+    }
+
+}
