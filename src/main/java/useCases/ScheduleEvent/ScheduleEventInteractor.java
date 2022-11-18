72aba4642ffca1373581fae428609e7ad6c092be
Vishnu Nittoor <nitvishn@gmail.com>
2022-11-17 22:31:21 -0500

    Committing to merge stashed changes to the ScheduleEventInteractor file.

---
+++ src/main/java/useCases/ScheduleEvent/ScheduleEventInteractor.java
@@ -0,0 +1,21 @@
+package useCases.ScheduleEvent;
+
+import entities.Event;
+import entities.User;
+import services.CurrentUserService;
+
+// TODO: document this class
+public class ScheduleEventInteractor {
+
+    CurrentUserService currentUserService;
+
+    ScheduleEventInteractor(CurrentUserService currentUserService){
+        this.currentUserService = currentUserService;
+    }
+
+    public Event scheduleEvent(ScheduleEventInputData inputData){
+
+        User user = currentUserService.getCurrentUser();
+
+    }
+}
