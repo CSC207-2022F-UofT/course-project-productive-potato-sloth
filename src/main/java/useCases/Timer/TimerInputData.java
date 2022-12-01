2a75e2101c59c207758be442148367a9b3542120
chazzybearr <leoncai@outlook.com>
2022-12-01 00:11:52 -0500

    Remove unmappable characters

--- src/main/java/useCases/Timer/TimerInputData.java
+++ src/main/java/useCases/Timer/TimerInputData.java
@@ -1,4 +1,5 @@
 package useCases.Timer;
+
 import java.time.Duration;
 
 /**
@@ -12,44 +13,50 @@ public class TimerInputData {
     private Duration inputDurationOfTimer;
 
     /**
-     * Stores Input string that depends on the user’s input
+     * Stores Input string that depends on the user's input
      * Input string like start, pause, end
      */
     private String userCommand;
 
     //constructor
+
     /**
      * Constructs TimerInputData object given the input duration
+     *
      * @param inputDuration The duration for the Timer, how long of a Timer session do we want
      */
-    TimerInputData(Duration inputDuration){
+    TimerInputData(Duration inputDuration) {
         this.inputDurationOfTimer = inputDuration;
     }
 
     //constructor 2
+
     /**
      * Constructs TimerInputData object given the input duration
+     *
      * @param inputDuration The duration for the Timer, how long of a Timer session do we want
-     * @param userCommand Stores Input string that depends on the user input, Input string like start, pause, end
+     * @param userCommand   Stores Input string that depends on the user input, Input string like start, pause, end
      */
-    TimerInputData(Duration inputDuration, String userCommand){
+    TimerInputData(Duration inputDuration, String userCommand) {
         this.inputDurationOfTimer = inputDuration;
         this.userCommand = userCommand;
     }
 
     /**
      * Gets the startTime of the Timer
+     *
      * @return startTime of the Timer, LocalDateTime when the timer was started
      */
-    Duration getInputDurationOfTimer(){
+    Duration getInputDurationOfTimer() {
         return this.inputDurationOfTimer;
     }
 
     /**
      * Gets the startTime of the Timer
+     *
      * @return startTime of the Timer, LocalDateTime when the timer was started
      */
-    String getUserCommand(){
+    String getUserCommand() {
         return this.userCommand;
     }
 }
