7801b84b5b8f39211004b85151738c53152b0c73
Dawei-He2002 <113304165+Dawei-He2002@users.noreply.github.com>
2022-12-02 12:46:06 -0500

    Added additional updateView method, associated updateViewInteractor with the UI and wrote additional tests.

--- src/test/java/useCases/ChatRoomInteractorTest.java
+++ src/test/java/useCases/ChatRoomInteractorTest.java
@@ -1,9 +1,14 @@
 package useCases;
+import controllers.ChatRoomControllers.UpdateViewPresenter;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.*;
 import entities.*;
 import useCases.ChatRoom.ChatRoomInteractor;
+import controllers.ChatRoomControllers.UpdateViewPresenterInterface;
+import useCases.responseModels.MessageResponseModel;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -11,6 +16,9 @@ public class ChatRoomInteractorTest {
     @Before
     public void setUp() {
     }
+    ChatRoom room = new ChatRoom();
+    ChatRoomInteractor interactor = new ChatRoomInteractor(room);
+    User user1 = new User();
 
     @After
     public void tearDown() {
@@ -18,11 +26,25 @@ public class ChatRoomInteractorTest {
 
     @Test
     public void testMessageToString(){
-        ChatRoom room = new ChatRoom();
-        ChatRoomInteractor interactor = new ChatRoomInteractor(room);
-        User user1 = new User();
+
         interactor.sendMessage("abcd", user1);
         //assertion
         assertEquals("chatRoom with messages [abcd]", room.toString());
+    }
+
+    @Test
+    public void testUpdateView(){
+        String[] str_array = new String[]{"a", "b", "c", "d", "e", "f"};
+        for(String i:str_array){
+            interactor.sendMessage(i, user1);
+        }
+        List<MessageResponseModel> list1 = interactor.updateView();
+        assertEquals(interactor.getMessageIndex(), 3);
+        assertEquals(list1.toString(), "[d, e, f]");
+    }
+
+    @Test
+    public void testInitializeView(){
+        //test not yet implemented because view initialization method needs to be moved into Main
     }
 }
