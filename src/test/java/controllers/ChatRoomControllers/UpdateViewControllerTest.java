9e4774094d0f1744fd032be71f5237de2550c630
Dawei-He2002 <113304165+Dawei-He2002@users.noreply.github.com>
2022-12-03 18:23:17 -0500

    Added tests for controllers.

---
+++ src/test/java/controllers/ChatRoomControllers/UpdateViewControllerTest.java
@@ -0,0 +1,27 @@
+package controllers.ChatRoomControllers;
+import UI.MainUI;
+import entities.ChatRoom;
+import entities.Message;
+import entities.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import useCases.ChatRoom.ChatRoomInteractor;
+import useCases.ChatRoom.ChatRoomInteractorInterface;
+
+public class UpdateViewControllerTest {
+    @Before
+    public void SetUp(){
+    }
+    ChatRoom room = new ChatRoom();
+    ChatRoomInteractorInterface interactor = new ChatRoomInteractor(room);
+    SendMessageController sender = new SendMessageController(interactor);
+    @After
+    public void TearDown(){
+    }
+    @Test
+    public void TestUpdateView(){
+        String[] str_list = new String{"a", "b", "c", "d", "e", "f"};
+    }
+}
