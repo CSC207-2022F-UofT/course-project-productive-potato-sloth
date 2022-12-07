90b091427eca70b6b7e62e2266b50250f0c6dcfa
Khai Lam Nguyen <105087569+kldtntg@users.noreply.github.com>
2022-12-07 16:41:30 -0500

    Delete src/main/java/UI directory

--- src/main/java/UI/sendInvitationSuccessfulScreen.java
+++
@@ -1,8 +0,0 @@
-package UI;
-
-public class sendInvitationSuccessfulScreen {
-    // display:
-        // You successfully sent an invitation for the task *task* to *receiver*
-
-        // *task* and *receiver* received from presenter.
-}
