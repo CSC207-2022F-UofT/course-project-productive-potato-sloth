cabf349bce7291b479f0af1d205d6d033ead6b0b
Khai Lam Nguyen <105087569+kldtntg@users.noreply.github.com>
2022-12-07 16:49:20 -0500

    Delete src/main/java/InvitationcontrollerAndPresenter directory

--- src/main/java/InvitationcontrollerAndPresenter/acceptInvitationPresenterOutput.java
+++
@@ -1,7 +0,0 @@
-package InvitationcontrollerAndPresenter;
-
-import useCases.InvitationAcceptOrDecline.AcceptInvitationOutputModel;
-
-public interface acceptInvitationPresenterOutput {
-    public void updateView(AcceptInvitationOutputModel outputModel);
-}
