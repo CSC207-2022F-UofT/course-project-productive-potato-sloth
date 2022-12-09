055aa81b369db38f0e9c454044b5a866d436bd7a
conve <chhavi.shah@mail.utoronto.ca>
2022-12-08 20:26:21 -0500

    more debugging stuff

--- src/main/java/screens/CreateAccount/CreateAccountMain.java
+++ src/main/java/screens/CreateAccount/CreateAccountMain.java
@@ -2,7 +2,6 @@ package screens.CreateAccount;
 
 import gateways.UserDataAccessInterface;
 import gateways.UserDatabaseGateway;
-import services.CurrentUserService;
 import useCases.CreateAccount.CreateAccountInputBoundary;
 import useCases.CreateAccount.CreateAccountInteractor;
 import useCases.CreateAccount.CreateAccountPresenter;
@@ -12,16 +11,13 @@ import java.awt.*;
 import java.io.IOException;
 
 public class CreateAccountMain {
-    public static void main(String[] args) throws IOException {
+    public static void main() throws IOException {
         JFrame application  =  new JFrame("Create Account");
         CardLayout cardLayout = new CardLayout();
         JPanel screens = new JPanel(cardLayout);
         application.add(screens);
 
         UserDataAccessInterface gateway = new UserDatabaseGateway("database/UserFile1.ser");
-        CurrentUserService service = new CurrentUserService();
-
-        System.out.println(gateway.getAll().get(0).getPassword());
 
         CreateAccountPresenter presenter = new CreateAccountResponseFormatter();
 
