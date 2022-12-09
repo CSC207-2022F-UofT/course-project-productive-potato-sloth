04100681872ad10cb786a013123ab0276999cbc7
jiang664 <yicheng.jiang@mail.utoronto.ca>
2022-12-09 03:31:44 -0500

    PUlled origin main

--- src/main/java/screens/Calculator/OutPutHistogram.java
+++ src/main/java/screens/Calculator/OutPutHistogram.java
@@ -14,30 +14,30 @@ public class OutPutHistogram {
     public void produce_graph() {
         String unit = "day";
         CurrentUserService service = new CurrentUserService();
-        User user = service.getCurrentUser();
-        TotCalculator calc = new TotCalculator();
-        int monday = calc.rawTime(user, unit);
-        int tuseday = calc.rawTime(user, unit);
-        int wednesday = calc.rawTime(user, unit);
-        int thursday = calc.rawTime(user, unit);
-        int friday = calc.rawTime(user, unit);
-        int saturday = calc.rawTime(user, unit);
-        int sunday = calc.rawTime(user, unit);
+//        User user = service.getCurrentUser();
+//        TotCalculator calc = new TotCalculator();
+//        int monday = calc.rawTime(user, unit);
+//        int tuseday = calc.rawTime(user, unit);
+//        int wednesday = calc.rawTime(user, unit);
+//        int thursday = calc.rawTime(user, unit);
+//        int friday = calc.rawTime(user, unit);
+//        int saturday = calc.rawTime(user, unit);
+//        int sunday = calc.rawTime(user, unit);
         HistogramPanel histogram = new HistogramPanel();
-            EventQueue.invokeLater(new Runnable()
-            {
-                public void run()
-                {
-                    createAndShowGUI(monday, tuseday, wednesday, thursday, friday, saturday, sunday);
-                }
-            });
-//        EventQueue.invokeLater(new Runnable()
-//        {
-//            public void run()
+//            EventQueue.invokeLater(new Runnable()
 //            {
-//                createAndShowGUI(1, 5, 10, 21, 4, 6, 10);
-//            }
-//        });
+//                public void run()
+//                {
+//                    createAndShowGUI(monday, tuseday, wednesday, thursday, friday, saturday, sunday);
+//                }
+//            });
+        EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                createAndShowGUI(1, 5, 10, 21, 4, 6, 10);
+            }
+        });
 
     }}
 
